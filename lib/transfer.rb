2a114e4ed540f3cb153fdd8951a8903ebd3f4bcf
Kayla Hansen <shiverr@gmail.com>
2017-10-22 21:16:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,10 @@ class Transfer
       sender.balance = (sender.balance - @amount)
       receiver.balance = (receiver.balance + @amount)
       self.status = "complete"
+        if self.status = "complete"
+          sender.balance
+          receiver.balance
+        end
   end
 end
 
