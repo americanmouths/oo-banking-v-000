38294b1022ecaf6cf8870d8b736c4448f514a961
Kayla Hansen <shiverr@gmail.com>
2017-10-22 21:05:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,6 +24,7 @@ class Transfer
       receiver.balance += self.amount
       self.status = "complete"
     else
+      !@sender.valid?
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
   end
